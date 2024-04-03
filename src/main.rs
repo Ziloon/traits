104a6653f96d5cdf8b088cd5872f894706f63b3c
Zhao Ziloong <ziloon.z@icloud.com>
2024-04-03 23:58:29 +0800

    创建trait，独立mod，使用泛型

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use traits::Anamal;
-
-
+mod zmath;
 
 fn main() {
     let bob = traits::Human::new(String::from("Bob"), 11);
     let gongzhu = traits::Cat::new(String::from("🐱"));
     println!("{}. Run {} m.", bob.ask(), bob.run());
     println!("{}. Run {} m.", gongzhu.ask(), gongzhu.run());
+
+    let l = vec![0,12,32,5435,65,5666];
+    let largNum = zmath::cmp::largest(&l);
+    let a = Option::Some(8);
 }
